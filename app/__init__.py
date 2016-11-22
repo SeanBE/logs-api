5df07c98ca3ceabe1b06f0057db17dc61d4f5064
seanBE <lofgren.sean@gmail.com>
2016-11-22 13:44:26 -0600

    add Access-Control-Allow options after each request

--- app/__init__.py
+++ app/__init__.py
@@ -4,17 +4,23 @@ from flask import Flask
 from app.database import db
 from app.api import blueprint as api
 
+# This the correct way??
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 def create_app(config=config.DevConfig):
     """
     Creates a new Flask application and initializes application.
     """
-
     app = Flask(__name__)
     app.config.from_object(config)
 
     db.init_app(app)
 
     app.register_blueprint(api, url_prefix='/api/1')
+    app.after_request_funcs = {None:[after_request]}
 
     return app
